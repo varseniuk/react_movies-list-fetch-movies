9a45eae2289b5b04435321e3dca7ec3739e25a48
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-09-12 16:58:50 +0300

    Updates

---
+++ precache-manifest.31ab48e023526cc12f366ecd0c9a87b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d263a5341f061a5c5bbe4d8e928ecf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6d8b1ff3fc1e85455bf",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "34ac217e98350d63e552",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "e6d8b1ff3fc1e85455bf",
+    "url": "./static/js/2.cbdf0394.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.cbdf0394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ac217e98350d63e552",
+    "url": "./static/js/main.687fc55d.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
